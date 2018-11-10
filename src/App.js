3c292ec6b29d80b2ee6036b80a1726a34f4f2e17
Nate Miller <natekmiller96@gmail.com>
2018-11-10 19:09:07 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    peopleInSpace: []
+  }
+ 
+  componentDidMount(){
+    this.handleAddTimer()
+  }
+
+  render() {
+    <div>
+      {this.state.peopleInSpace.map(person => person.name)}
+    </div>
+  )
+  }
+
+
+export default App;
+
